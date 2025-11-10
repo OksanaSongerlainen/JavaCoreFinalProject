262f9d464c942b1a051f6ca3979fe43d4b49088d
OSongerlainen <9779589@inbox.ru>
2025-11-10 15:23:01 +0300

    New project

---
+++ src/model/Account.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Account {
+    private String accountNumber;
+    private int balance;
+
+    public Account(String accountNumber, int balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
